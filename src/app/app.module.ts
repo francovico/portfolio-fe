ed9967b3cb862a39a02e6be026ab5469ac06cb6e
francovico <francoivico@gmail.com>
2022-07-11 18:28:52 -0300

    Saque boton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { BotonAgregarComponent } from './boton-agregar/boton-agregar.component';
 import { LogoComponent } from './logo/logo.component';
 import { SocialComponent } from './social/social.component';
 import { BannerComponent } from './banner/banner.component';
 import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BotonAgregarComponent,
     LogoComponent,
     SocialComponent,
     BannerComponent,
-    AboutComponent
+    AboutComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule,
