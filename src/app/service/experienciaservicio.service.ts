dd2d553b48946c3134131a356c37afbfcfce467f
francovico <francoivico@gmail.com>
2022-07-26 00:30:37 -0300

    Update experienciaservicio.service.ts

--- src/app/service/experienciaservicio.service.ts
+++ src/app/service/experienciaservicio.service.ts
@@ -7,7 +7,7 @@ import { Experiencia } from '../model/experiencia';
   providedIn: 'root'
 })
 export class ExperienciaservicioService {
-  expURL = 'https://portfolio-be-francovico.herokuapp.com/experiencia/';
+  expURL = 'http://portfolio-be-francovico.herokuapp.com/experiencia/';
 
   constructor(private httpClient: HttpClient) { }
 
