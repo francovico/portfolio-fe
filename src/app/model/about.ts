e2a85088e2e08391beb2ee9ae16f02e6ea52b9dd
francovico <francoivico@gmail.com>
2022-07-26 17:59:51 -0300

    Agregado de Job

--- src/app/model/about.ts
+++ src/app/model/about.ts
@@ -1,8 +1,10 @@
 export class About {
   id? : number;
   about : string;
+  job: string;
 
-  constructor(about: string){
+  constructor(about: string, job: string){
       this.about = about;
+      this.job = job;
   }
 }
