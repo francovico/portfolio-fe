8f6696b9c5288edcdca4489bdd297d4162ab101b
francovico <francoivico@gmail.com>
2022-07-26 14:36:06 -0300

    Sacando proxy inverso y modificando CORS

--- server.js
+++ server.js
@@ -3,25 +3,6 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-//
-const whitelist = ['https://portfolio-be-francovico.herokuapp.com/']; // list of allow domain
-
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin) {
-            return callback(null, true);
-        }
-
-        if (whitelist.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}
-//
-
 app.use(cors());
 
 const path = require('path');
@@ -34,4 +15,4 @@ app.get('/*', (req, res) =>
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 80);
